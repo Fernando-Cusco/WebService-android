c54f49cdd293147c6a2189c31620e6fb19d48db4
Fernando Cusco <bcuscom@est.ups.edu.ec>
2019-10-12 00:57:21 -0500

    [add] second commit

--- app/src/main/java/ups/sistemas/webservice/MainActivity.java
+++ app/src/main/java/ups/sistemas/webservice/MainActivity.java
@@ -3,6 +3,7 @@ package ups.sistemas.webservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        Toast.makeText(this, "Hola", Toast.LENGTH_SHORT).show();
     }
 }
